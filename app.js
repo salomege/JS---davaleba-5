eb6292d70fac1be830d7944bcce98f4c64bb1410
salomege <salomegelenidze@gmail.com>
2021-11-04 17:54:21 +0400

    create part of the homework

---
+++ app.js
@@ -0,0 +1,13 @@
+//1
+const mybtn = document.querySelector ('.mybtn');
+   
+mybtn.addEventListener(`click`,function(){
+    mybtn.remove();
+})
+
+//2
+var img = document.createElement('img');
+img.src = 'https://i.ytimg.com/vi/VEx5hNYeZtY/hqdefault.jpg';
+document.getElementById('jsImage').appendChild(img)
+
+//3
